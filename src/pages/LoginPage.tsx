0ed2d8d28513b4eb231fbc48cd8ce5a026291e30
EZIC13 <80130551+EZIC13@users.noreply.github.com>
2026-04-07 19:44:37 -0600

    set colors for login and register pages

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -38,41 +38,41 @@ const LoginPage = () => {
         <div className="flex min-h-screen flex-col justify-center px-6 py-12 lg:px-8 bg-white">
 
             <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-                <img className="mx-auto h-10 w-auto" src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600" alt="Site Logo"/>
-                <h2 className="mt-10 text-center text-2xl font-bold tracking-tight text-gray-900">Sign in to your account</h2>
+                {/*<img className="mx-auto h-10 w-auto" src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600" alt="Site Logo"/>*/}
+                <h2 className="mt-10 text-center text-2xl font-bold tracking-tight text-cb-black">Sign in to your account</h2>
             </div>
 
             <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
                 <form className="space-y-6" onSubmit={handleSubmit}>
 
                     <div>
-                        <label className="block text-sm font-medium text-gray-900">Username</label>
+                        <label className="block text-sm font-medium text-cb-black">Username</label>
                         <div className="mt-2">
-                            <input value={username} onChange={(e) => setUsername(e.target.value)} type="text" required className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:outline-indigo-600 sm:text-sm"/>
+                            <input value={username} onChange={(e) => setUsername(e.target.value)} type="text" required className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-cb-black outline-1 outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:outline-cb-blue sm:text-sm"/>
                         </div>
                     </div>
 
                     <div>
                         <div className="flex items-center justify-between">
-                            <label className="block text-sm font-medium text-gray-900">Password</label>
+                            <label className="block text-sm font-medium text-cb-black">Password</label>
                             <div className="text-sm">
-                                <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
+                                <a href="#" className="font-semibold text-cb-blue hover:text-cb-blue-hover">Forgot password?</a>
                             </div>
                         </div>
                         <div className="mt-2">
-                            <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" required className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:outline-indigo-600 sm:text-sm"/>
+                            <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" required className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-cb-black outline-1 outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:outline-cb-blue sm:text-sm"/>
                         </div>
                     </div>
 
                     <div>
-                        <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+                        <button type="submit" className="flex w-full justify-center rounded-md bg-cb-blue px-3 py-1.5 text-sm font-semibold text-white shadow-xs hover:bg-cb-blue-hover focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cb-blue">Sign in</button>
                     </div>
                 </form>
             </div>
 
             <p className="mt-10 text-center text-sm text-gray-400">
                 Don&apos;t have an account?{" "}
-                <a href="/register" className="font-semibold text-indigo-600 hover:text-indigo-500">Register</a>
+                <a href="/register" className="font-semibold text-cb-blue hover:text-cb-blue-hover">Register</a>
             </p>
         </div>
     );
